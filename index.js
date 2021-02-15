e234f3ef55e90920de71ebb185bcc116043f4de6
Melissa Faulner <melissa.faulner@gmail.com>
2021-02-14 19:26:00 -0500

    all mvp finished except delete and user login

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 madLibURL = 'http://localhost:3000/mad_libs'
 textEntryURL = 'http://localhost:3000/text_entries/'
+userURL = 'http://localhost:3000/users'
 
 // renderMadLibs().then(renderAMadlib)
+let userId = 1
 const storyContainer = document.querySelector('#mad-libs-story')
 const inputForm = document.querySelector('#input-form')
 const saveForm = document.querySelector('#save-your-mad-lib')
 const cardsContainer = document.querySelector('#cards-container')
+const ssUL = document.querySelector('#ssUL')
+
+fetchUserTextEntries().then(populateSavedStories)
+
+changeFormIDs(userId)
+function changeFormIDs(userId){
+    inputForm.number.value = userId
+}
+
+function populateSavedStories(text_entries){
+    ssUL.innerHTML = ""
+    text_entries.user.forEach(entry => {
+        li = document.createElement("li")
+        li.dataset.id = entry.mad_lib_id
+        li.dataset.input = entry.id
+        li.innerText = `${entry.title}`
+        li.addEventListener('click', showSavedStory)
+        ssUL.append(li)
+    });
+}
+
+function showSavedStory(e){
+    console.log(e.target.dataset);
+    const madlibID = parseInt(e.target.dataset.id)
+    const otherID = e.target.dataset.input
+    fetch(userURL + `/${userId}`+`/${otherID}`)
+    .then(response => response.json())
+    .then(response => {
+        let responses = response.inputs.split(",")
+        let response1 = responses[0]
+        let response2 = responses[1]
+        let response3 = responses[2]
+        let response4 = responses[3]
+        let story = response.mad_story
+    storyContainer.innerHTML = ""
+    let div = document.createElement("div")
+    div.className = "current-story"
+    div.innerHTML = story
+    storyContainer.append(div)
+    const input1 = document.querySelector('#input1')
+    const input2 = document.querySelector('#input2')
+    const input3 = document.querySelector('#input3')
+    const input4 = document.querySelector('#input4')
+    let target1 = response1
+    let target2 = response2
+    let target3  = response3
+    let target4 = response4
+    input1.innerHTML = target1
+    input2.innerHTML = target2
+    input3.innerHTML = target3
+    input4.innerHTML = target4
+    storyContainer.className = "not-hidden"
+    })
+}
 
 // ********** Event Listeners **********
 inputForm.addEventListener('submit', updateStory)
@@ -13,6 +69,12 @@ cardsContainer.addEventListener('click', fetchAMadlib)
 saveForm.addEventListener('submit', saveStory)
 
 // ********** Functions **********
+
+function fetchUserTextEntries() {
+    return fetch(userURL + `/${userId}/text`)
+    .then(response => response.json())
+}
+
 function renderMadLibs() {
     return fetch(madLibURL)
     .then(response => response.json())
@@ -25,7 +87,7 @@ function fetchAMadlib(e) {
     return fetch(madLibURL + `/${hTMLID}`)
     .then(response => response.json())
     .then(renderAMadlib)
-    }
+}
 } 
 
 function renderAMadlib(madLib){
@@ -33,27 +95,28 @@ function renderAMadlib(madLib){
     inputForm.innerHTML = ""
     storyContainer.innerHTML = ""
     let div = document.createElement("div")
-        div.className = "current-story"
-        div.dataset.id = madLib.id
-        div.innerHTML = madLib.story
-        storyContainer.append(div)
+    div.className = "current-story"
+    div.dataset.id = madLib.id
+    div.innerHTML = madLib.story
+    storyContainer.append(div)
     const mad_lib_id = parseInt(document.querySelector('.current-story').dataset.id)
     inputForm.innerHTML += madLib.form
     inputForm.className = "not-hidden"
     saveForm.className = "hide"
+    changeFormIDs(userId)
 }
 
 function updateStory(e){
     e.preventDefault()
-
+    
     const input1 = document.querySelector('#input1')
     const input2 = document.querySelector('#input2')
     const input3 = document.querySelector('#input3')
     const input4 = document.querySelector('#input4')
-    let target1 = e.target[0].value
-    let target2 = e.target[1].value
-    let target3  = e.target[2].value
-    let target4 = e.target[3].value
+    let target1 = e.target[1].value
+    let target2 = e.target[2].value
+    let target3  = e.target[3].value
+    let target4 = e.target[4].value
     input1.innerHTML = target1
     input2.innerHTML = target2
     input3.innerHTML = target3
@@ -66,10 +129,9 @@ function updateStory(e){
 
 function saveStory(e) {
     e.preventDefault()
-    console.log(e);
     const mad_lib_id = parseInt(document.querySelector('.current-story').dataset.id)
     const title = e.target[0].value
- 
+    
     const input1 = document.querySelector('#input1')
     const input2 = document.querySelector('#input2')
     const input3 = document.querySelector('#input3')
@@ -87,13 +149,21 @@ function saveStory(e) {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(postObj)
-})
-saveForm.className = "hide"
+    })
+    .then(response => response.json())
+    .then(response => {
+        li = document.createElement("li")
+        li.dataset.id = mad_lib_id
+        li.dataset.input = response.id
+        li.innerText = `${title}`
+        li.addEventListener('click', showSavedStory)
+        ssUL.append(li)
+    })
+    saveForm.className = "hide"
 }
 
-const userid = 1
-changeFormIDs(userid)
-function changeFormIDs(userId){
-    console.log(userId);
-    inputForm.number.value = userId
-}
+try {
+    Typekit.load({
+        async: true
+	});
+} catch (e) {}
